29261257d2445c19f750ca3dea381ab8665ca0f2
a.gheynarloo <a.gheynarloo@hamisystem.ir>
2021-03-17 02:18:46 +0330

    split components in project the law DRY

--- src/helpers/dark-mode.helper.ts
+++ src/helpers/dark-mode.helper.ts
@@ -3,8 +3,16 @@ import { LocalstorageHelper } from "./localstorage.helper";
 export class DarkModeHelper {
 
     private _localstorageHelper = new LocalstorageHelper();
+    /**
+     * key in darkmode in localstorage
+     */
     private _dark_model_label = "dark-mode";
 
+    /**
+     * add class dark in html
+     * storage status in localstorage
+     * @param status 
+     */
     darkMode(status: boolean = true) {
         if (status === true) {
             document.documentElement.classList.remove("dark");
@@ -14,6 +22,10 @@ export class DarkModeHelper {
         this._localstorageHelper.setItem(this._dark_model_label, status);
     }
 
+    /**
+     * return darkmode status with localstorage
+     * @returns 
+     */
     status(): boolean {
         return this._localstorageHelper.getItem<boolean>(this._dark_model_label) ?? false;
     }
